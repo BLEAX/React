02c390c79bd2379772ab8e6dcbe7ae790118a4db
kimjeonghun <109516312+BLEAX@users.noreply.github.com>
2024-05-28 17:06:52 +0900

    Add files via upload

---
+++ 과제/소프트웨어학과 홈페이지/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './page/Header.jsx';
+import Footer from './page/Footer.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
